faf003667b9f2fabd2e8ffd5fd195529c1924ea8
Cache McClure <cache@horizonanalyticstudios.com>
2026-04-18 15:58:53 -0500

    Added Cloudflare functions to properly handle API proxying

---
+++ functions/api/horizons.ts
@@ -0,0 +1,16 @@
+export const onRequest: PagesFunction = async ({ request }) => {
+	const url = new URL(request.url);
+	const upstream = new URL('https://ssd.jpl.nasa.gov/api/horizons.api');
+	upstream.search = url.search;
+
+	const response = await fetch(upstream.toString());
+	const body = await response.text();
+
+	return new Response(body, {
+		status: response.status,
+		headers: {
+			'Content-Type': response.headers.get('Content-Type') || 'text/plain',
+			'Access-Control-Allow-Origin': '*',
+		},
+	});
+};
